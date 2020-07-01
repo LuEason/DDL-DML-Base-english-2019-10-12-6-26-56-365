6eb2d5d4d3a10257319ee662a6c7e95bae21f5d0
Eason <chenzscd@163.com>
2020-07-01 14:07:52 +0800

    finish

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use nebula;
 -- Create a database
-
+create database hw;
 -- Create the database of the designated character set
-
--- Display the creation information fo the database
-
+create database hw default character set = 'utf8';
+-- Display the creation information of the database
+show variables;
 -- Revise the codes of the database
 
 -- Delete a database
-
+drop database hw;
 -- **Table level**
 -- Revise table name
-
+alter table student rename to student1;
 -- Revise the field's data type
-
+alter table student modify id varchar(20);
 -- Revise field name
-
+alter table student rename column id to stu_id;
 -- Add field
-
+alter table student add stu_name varchar(10);
 -- Delete field
-
+alter table student drop column stu_name;
 -- Revise the table's storage engine
-
+alter table student engine=innodb;
 -- Delete the table's foreign key restriant
-
+alter table student drop foreign key FK_Reference_106;
 -- Delete a table
+drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into student (1, 'Eason');
 -- Revise record
-
+update student set id = 2 where id = 1
 --  Delete record
-
+delete from student where id = 2
 -- Search record
+select * from student where id = 2
